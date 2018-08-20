6ff85553002774521e5916700b94257091b64c9a
Anuu Thomson <anusanth94@gmail.com>
2018-08-20 18:42:52 +0200

    Update

---
+++ untitled/src/Statistics.java
@@ -0,0 +1,19 @@
+public class Statistics {
+    private double []numbers = null;
+    private int last = 0;
+
+
+    public Statistics( int size ) {
+        numbers = new double [ size ];
+    }
+
+    public void addNumber( double n ) {
+        numbers[last++] = n;
+    }
+
+    public double getAverage() {
+        double sum = 0;
+        for (int i = 0; i < last; i++) sum += numbers[i];
+        return sum/last;
+    }
+}
